f21e97cecfb1e661a0ba9e6a63de5c405fc3ea86
teran210c <a242729@uach.mx>
2025-12-05 21:05:21 -0700

    Feat: created api.js file where I will creat all my endpoints calls and an add week button to add weeks

--- src/components/Workouts.jsx
+++ src/components/Workouts.jsx
@@ -1,107 +1,27 @@
 import { useEffect, useState } from "react"
+import { postWeek } from "../api"
 
 export default function Workouts() {
-    const [workouts, setWorkouts] = useState([])
-    const [names, setNames] = useState({})
+    const [weeks, setWeeks] = useState([])
 
-    useEffect(() => {
-        fetch("http://localhost:8080/workouts")
-            .then((res) => res.json())
-            .then((json) => {
-                setWorkouts(json)
-            })
-    }, [])
-
-    async function createWorkout() {
-        const response = await fetch("http://localhost:8080/workouts", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({})
-        });
-
-        const data = await response.json()
-        console.log("Workout creado:", data)
-        setWorkouts(prev => [...prev, data])
+    async function addWeek() {
+        const newWeek = await postWeek()
+        setWeeks(prev => [...prev, newWeek])
     }
-
-    async function updateName(id, name) {
-        const response = await fetch(`http://localhost:8080/exercises/${id}/name`, {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                newName: name
-            })
-        })
-        const updatedExercise = await response.json();
-        setWorkouts(prev =>
-            prev.map(workout => ({
-                ...workout,
-                exercises: workout.exercises?.map(exercise =>
-                    exercise.id === updatedExercise.id ? updatedExercise : exercise
-                )
-            }))
-        )
-
-
-    }
-
 
 
     return (
-        <div>
-            <h1>Workouts</h1>
-            <button onClick={createWorkout}>Add Workout</button>
-            <ol>
-                {workouts.map((workout, index) => (
-                    <table key={index}>
-                        <thead>
-                            <tr>
-                                <th>{workout.id}</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {workout.exercises?.map((exercise, i) => (
-                                <tr key={i}>
-                                    <td>{i + 1}</td>
-                                    <td>{exercise.name === null ? (
-                                        <>
-                                            <input
-                                                type="text"
-                                                value={names[exercise.id] || ""}
-                                                onChange={(e) => setNames(prev => ({ ...prev, [exercise.id]: e.target.value }))}
-                                            />
-                                            <button onClick={() => updateName(exercise.id, names[exercise.id])}>
-                                                Guardar
-                                            </button>
+        <>
+            <h1>GYM app</h1>
+            <button onClick={addWeek}>Add week</button>
 
-                                        </>
-
-                                    ) : (
-                                        <>
-                                            {exercise.name}
-                                        </>
-                                        
-                                    )}</td>
-                                </tr>
-                            ))}
-                            <tr>
-                                <td>
-                                    <button>+</button>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+            <ul>
+                {weeks.map(week => (
+                    <li key={week.id}>{week.title}</li>
                 ))}
-            </ol>
+            </ul>          
 
 
-
-
-        </div>
-
+        </>
     )
 }
